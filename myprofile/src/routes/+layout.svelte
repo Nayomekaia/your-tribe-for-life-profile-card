e0460ec233ddd3aee453f3d9ae7351ef07003c62
Nayomekaia <nayomedoelwijt@gmail.com>
2025-09-12 11:23:42 +0200

    opzet layout

--- myprofile/src/routes/+layout.svelte
+++ myprofile/src/routes/+layout.svelte
@@ -1,12 +1,8 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
 	import '../app.css'; 
-	export let children;
   </script>
   
-  <svelte:head>
-	<link rel="icon" href={favicon} />
-  </svelte:head>
+ 
   
   <slot />
   
