b82d6664cb8b052942c6b07efc26bc9dbef35b6b
Nayomekaia <nayomedoelwijt@gmail.com>
2025-09-12 11:21:05 +0200

    nvt

---
+++ myprofile/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+	import '../app.css'; 
+	export let children;
+  </script>
+  
+  <svelte:head>
+	<link rel="icon" href={favicon} />
+  </svelte:head>
+  
+  <slot />
+  
