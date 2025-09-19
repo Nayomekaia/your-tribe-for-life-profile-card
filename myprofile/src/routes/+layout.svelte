17675a6d041ccd8f9447609a5aa9594f008932da
Nayomekaia <nayomedoelwijt@gmail.com>
2025-09-19 13:56:56 +0200

    Update +layout.svelte

--- myprofile/src/routes/+layout.svelte
+++ myprofile/src/routes/+layout.svelte
@@ -1,25 +1,9 @@
 <script>
-  import '$lib'
 	import '../app.css'; 
   </script>
   
-
-  <main>
-    <slot />
+ 
+</main>  
+  <slot />
   </main>
-   
-<style>
-
-  /* maak deze selectors global zodat ze NIET gescope’d worden */
-  :global(body) {
-  background-image: url('/concreet8.png');
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-position: center;
-  background-size: cover;
-
-
-}
-
-
-</style>
+  
